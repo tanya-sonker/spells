80adef368f5d2295cc3ebf03f0a9353639e621ee
Tanya Sonker <sonkercodes@gmail.com>
2026-06-06 23:55:01 +0530

    fix: disable gh workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = process.env.VERCEL ? '/' : '/spells/'
+
 export default defineConfig({
-  base: '/spells/',
+  base,
   plugins: [react()],
 })
